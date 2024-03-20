059ba6aaa258e638bca4f9d06902432cf7d56c50
Handi Priyono <handimacbook@gmail.com>
2024-03-20 21:12:42 +0700

    feat: add types

---
+++ src/pages/customer-management/types/index.ts
@@ -0,0 +1,10 @@
+export type TCustomerItem = {
+  no?: number;
+  id?: number;
+  name: string;
+  email: string;
+  username: string;
+  address: string;
+  status?: string | number;
+  totalOrder?: string;
+};
